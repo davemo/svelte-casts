5939936128b3c1f57201320fab2619c58e4dfb76
David Mosher <davidmosher@gmail.com>
2019-08-28 15:03:47 -0400

    refactor to component and store step 1:
    - passing users by reference as a prop, hardcoding the users within
    - first introduction of the export syntax for defining component 
    boundaries
    - using collection.subscribe manually to get a shallow copy of the
    collection, could probably refactor this to $collection? to use the
    magic auto de-referencing subscribe and cleanup with onDestroy
    - domain logic for selecting an item still lives in the component, which
    is not great — a future refactor will move this into the store itself

---
+++ svelte-components-and-stores/stores/users.store.js
@@ -0,0 +1,11 @@
+import { writable } from 'svelte/store';
+
+export const users = writable([
+  {name: 'Danika Dywtgowm', email: 'danika.dywtgowm@email.com'},
+  {name: 'Erica Bule', email: 'erica.bule@email.com'},
+  {name: 'Jim Snales', email: 'jim.snales@email.com'},
+  {name: 'Daria Thorobox', email: 'daria.thorobox@email.com'},
+  {name: 'Mendikant Hargrove', email: 'mendikant.hargrove@email.com'},
+  {name: 'Ephraim Lischok', email: 'ephraim.lischok@email.com'},
+  {name: 'Lera Nedialkova', email: 'lera.nedialkova@email.com'},
+])
